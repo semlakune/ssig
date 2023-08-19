898c0ba4d30fc04c2d548da4213fe129ec76a72c
Aji <aji@klob.id>
2023-08-19 19:39:21 +0700

    initial commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,112 @@
-import Image from 'next/image'
+'use client'
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+  const [value, setValue] = useState(1);
+  const [isValidValue, setIsValidValue] = useState(true);
+  const [isDownloading, setIsDownloading] = useState(false);
+  const [downloadLink, setDownloadLink] = useState("");
+  const [lastTotalSize, setLastTotalSize] = useState(0);
+
+  const generateImageFile = async () => {
+    try {
+      setDownloadLink("");
+      setIsDownloading(true);
+
+      const canvas = document.createElement("canvas");
+      const ctx = canvas.getContext("2d");
+
+      const imageSizeBytes = value * 1024 * 1024;
+      const targetSizeBytes = imageSizeBytes * 4.68; // Adjusted factor based on compression 11.572
+
+      const imageWidth = Math.floor(Math.sqrt(targetSizeBytes / 4)); // Each pixel requires 4 bytes (RGBA)
+      const imageHeight = Math.ceil(targetSizeBytes / (4 * imageWidth));
+
+      canvas.width = imageWidth;
+      canvas.height = imageHeight;
+
+      const imageData = ctx.createImageData(imageWidth, imageHeight);
+
+      // Fill image data with random values
+      for (let i = 0; i < imageData.data.length; i++) {
+        imageData.data[i] = Math.floor(Math.random() * 256);
+      }
+
+      ctx.putImageData(imageData, 0, 0);
+
+      const a = document.createElement("a");
+      a.href = canvas.toDataURL("image/jpeg");
+      a.download = `generated_image_${value}MB.jpg`;
+      setLastTotalSize(value);
+      setDownloadLink(a.href);
+
+      setTimeout(() => {
+        URL.revokeObjectURL(a.href);
+        setIsDownloading(false);
+      }, 100);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    setIsValidValue(value >= 1 && value <= 100);
+  }, [value]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
+      <div className="w-screen h-screen flex justify-center items-center flex-col gap-5">
+        <h1 className="text-xl font-bold">Size Specific Image Generator</h1>
+        <p className="w-1/4 text-center mb-[50px]">
+          The Size Specific Image Generator is a tailored tool designed for testers in need of images with precise dimensions. It efficiently generates images that meet specific size requirements, ensuring accurate testing without the hassle of manual adjustments.
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+        <div className="w-1/4 flex flex-col gap-5">
+          <div className="flex items-center justify-between w-[100%]">
+            <input
+                className="border px-3 py-2 rounded-md w-[90%]"
+                type="number"
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+                min={1}
             />
-          </a>
+            <p className="text-sm text-end font-bold w-[10%]">MB</p>
+          </div>
+          <button
+              className={`w-[100%] rounded-md px-3 py-2 text-white transition-all ${
+                  isValidValue ? "bg-blue-600 hover:scale-105" : "bg-slate-600 cursor-not-allowed"
+              }`}
+              onClick={async () => {
+                if (isValidValue) {
+                  await generateImageFile();
+                }
+              }}
+          >
+            {isDownloading ? "Generating..." : "Generate and Download Image"}
+          </button>
+          {isDownloading ? <p>Generating image...</p> : null}
+          {downloadLink && !isDownloading ? (
+              <a
+                  href={downloadLink}
+                  download
+                  onClick={() => setDownloadLink(null)}
+                  className="w-[100%] rounded-md px-3 py-2 text-white bg-emerald-600 text-center"
+              >
+                Download {lastTotalSize}MB
+              </a>
+          ) : null}
+        </div>
+        <div className="absolute bottom-10">
+          <h1 className="text-sm font-light">
+            Made with ☕️ by{" "}
+            <span
+                onClick={() => router.push("https://ajipurnomo.dev")}
+                className="cursor-pointer hover:text-sky-800 hover:font-bold hover:animate-ping transition-all"
+            >
+            Aji
+          </span>
+          </h1>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+  );
 }
